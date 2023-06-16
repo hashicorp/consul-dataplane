cf8da5206e022daffedbad8464c248f02c936188
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2023-06-16 16:53:02 -0400

    Backport of fix: log level present in extra args using envoy-extra-args annotation : NET-2190 into release/1.2.x (#151)
    * backport of commit 112da6e3f9751b548bcf22f45137e9efd291da38
    
    * backport of commit 10ce884e67019cca739e43ef0e1faaa76354ccf1
    
    * backport of commit 83203738419ce4b8b9746daf56cf53454aa17ba3
    
    * backport of commit bfe205784c0f59a9a9afd0575afc37979971b4a3
    
    * backport of commit 5f693b877d7bf2f84f18a73a93c113b0e733ca86
    
    * backport of commit 65ba8a37cde10af69b49bee844ec6524f957f589
    
    * backport of commit befaf13a48d2f0d6df5876fe982e2ee9d0156448
    
    ---------
    
    Co-authored-by: Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
    Co-authored-by: Curt Bushko <cbushko@gmail.com>

--- pkg/envoy/proxy_test.go
+++ pkg/envoy/proxy_test.go
@@ -146,3 +146,129 @@ func testOutputPath() string {
 		fmt.Sprintf("test-output-%x.json", time.Now().UnixNano()+int64(os.Getpid())),
 	)
 }
+
+func TestProxy_OverridingLoggerAndExtraArgs(t *testing.T) {
+	bootstrapConfig := []byte(`hello world`)
+
+	// Test checks we are starting proxy with only one argument of log-level
+	// When log-level is present in both ProxyConfig and ExtraArgs
+	// We override the log-level with that of ExtraArgs
+	outputPath := testOutputPath()
+	t.Cleanup(func() { _ = os.Remove(outputPath) })
+
+	p, err := NewProxy(ProxyConfig{
+		Logger:            hclog.New(&hclog.LoggerOptions{Level: hclog.Warn, Output: io.Discard}),
+		EnvoyErrorStream:  io.Discard,
+		EnvoyOutputStream: io.Discard,
+		ExecutablePath:    "testdata/fake-envoy",
+		ExtraArgs:         []string{"--test-output", outputPath, "--log-level", "debug"},
+		BootstrapConfig:   bootstrapConfig,
+	})
+	require.NoError(t, err)
+	require.NoError(t, p.Run(context.Background()))
+	t.Cleanup(func() { _ = p.Kill() })
+
+	// Read the output written by fake-envoy. It might take a while, so poll the
+	// file for a couple of second
+	var output struct {
+		Args       []byte
+		ConfigData []byte
+	}
+	require.Eventually(t, func() bool {
+		outputBytes, err := os.ReadFile(outputPath)
+		if err != nil {
+			t.Logf("failed to read output file: %v", err)
+			return false
+		}
+		if err := json.Unmarshal(outputBytes, &output); err != nil {
+			t.Logf("failed to unmarshal output file: %v", err)
+			return false
+		}
+		return true
+	}, 2*time.Second, 50*time.Millisecond)
+
+	// Check that fake-envoy was able to read the config from the pipe.
+	require.Equal(t, bootstrapConfig, output.ConfigData)
+
+	// Check that we're correctly configuring the log level.
+	require.Contains(t, string(output.Args), "--log-level debug")
+
+	// Check that we're removing the logger in Proxy config and overriding it with extra args
+	require.NotContains(t, string(output.Args), "--log-level warn")
+
+	// Check that we're disabling hot restarts.
+	require.Contains(t, string(output.Args), "--disable-hot-restart")
+
+	// Check the process is still running.
+	require.NoError(t, p.cmd.Process.Signal(syscall.Signal(0)))
+
+	// Ensure Stop kills and reaps the process.
+	require.NoError(t, p.Kill())
+
+	require.Eventually(t, func() bool {
+		return p.cmd.Process.Signal(syscall.Signal(0)) == os.ErrProcessDone
+	}, 2*time.Second, 50*time.Millisecond)
+}
+
+func TestProxy_EnvoyExtraArgs(t *testing.T) {
+	bootstrapConfig := []byte(`hello world`)
+
+	// Test checks we are starting proxy with only one argument of log-level
+	// When log-level is present in both ProxyConfig and ExtraArgs
+	// We override the log-level with that of ExtraArgs
+	outputPath := testOutputPath()
+	t.Cleanup(func() { _ = os.Remove(outputPath) })
+
+	p, err := NewProxy(ProxyConfig{
+		Logger:            hclog.New(&hclog.LoggerOptions{Level: hclog.Warn, Output: io.Discard}),
+		EnvoyErrorStream:  io.Discard,
+		EnvoyOutputStream: io.Discard,
+		ExecutablePath:    "testdata/fake-envoy",
+		ExtraArgs:         []string{"--test-output", outputPath, "--log-level", "debug", "--concurrency", "1"},
+		BootstrapConfig:   bootstrapConfig,
+	})
+	require.NoError(t, err)
+	require.NoError(t, p.Run(context.Background()))
+	t.Cleanup(func() { _ = p.Kill() })
+
+	// Read the output written by fake-envoy. It might take a while, so poll the
+	// file for a couple of second
+	var output struct {
+		Args       []byte
+		ConfigData []byte
+	}
+	require.Eventually(t, func() bool {
+		outputBytes, err := os.ReadFile(outputPath)
+		if err != nil {
+			t.Logf("failed to read output file: %v", err)
+			return false
+		}
+		if err := json.Unmarshal(outputBytes, &output); err != nil {
+			t.Logf("failed to unmarshal output file: %v", err)
+			return false
+		}
+		return true
+	}, 2*time.Second, 50*time.Millisecond)
+
+	// Check that fake-envoy was able to read the config from the pipe.
+	require.Equal(t, bootstrapConfig, output.ConfigData)
+
+	// Check that we're correctly configuring the log level.
+	require.Contains(t, string(output.Args), "--log-level debug")
+
+	// Check that we're removing the logger in Proxy config and overriding it with extra args
+	require.NotContains(t, string(output.Args), "--log-level warn")
+
+	// Check that we're disabling hot restarts.
+	require.Contains(t, string(output.Args), "--disable-hot-restart")
+
+	// Check the process is still running.
+	require.NoError(t, p.cmd.Process.Signal(syscall.Signal(0)))
+
+	// Ensure Stop kills and reaps the process.
+	require.NoError(t, p.Kill())
+
+	require.Eventually(t, func() bool {
+		return p.cmd.Process.Signal(syscall.Signal(0)) == os.ErrProcessDone
+	}, 2*time.Second, 50*time.Millisecond)
+}
