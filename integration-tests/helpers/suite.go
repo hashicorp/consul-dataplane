0171e1a6405ea4d1dd411b6a8a02e1b4be51dc6f
Curt Bushko <cbushko@gmail.com>
2023-06-27 12:27:57 -0400

    proxy-lifecycle: catch SIGTERM and initiate graceful shutdown (#130) (#183)
    * makefile: bump default integration tests server image to hashicorppreview/consul:1.15-dev
    
    * integration-tests: add upstream for frontend service to backend
    
    * integration-tests: run frontend service and setup two-way allow intentions
    
    * cmd: initiate graceful shutdown from SIGTERM
    
    * add TODOs for testing container termination handling
    
    * test: add graceful shutdown CLI flags to RunContainer integration test helper
    
    * test: configure graceful shutdown for frontend in integration tests
    
    remove k8s-specific lifecycle TODOs that differentiate container shutdown from pod
    termination
    
    * test: make graceful shutdown configuration optional in dataplane helper
    
    * restore SIGINT and SIGTERM handling
    
    * pkg/consuldp: cancel parent context and exit if proxy lifecycle manager never started
    
    * test: fixup ShutdownDrainListeners bool config, send SIGTERM to frontend sidecar container
    
    * pkg/envoy: start Envoy in its own process group to avoid directly sending SIGTERM
    
    Let consul-dataplane handle the signal and control graceful shutdown.
    
    * ci: set DOCKER_API_VERSION to avoid error
    
    * test: negotiate Docker API version
    
    * pkg/consuldp/lifecycle: call gracefulShutdown from HTTP endpoint handler in a separate goroutine to avoid blocking
    
    test: check proxy manager mock async now that HTTP endpoint is non-blocking
    
    test: add shutdownTimeout var for shutdownGracePeriod plus fuzzy allowance
    
    * integration-tests: skip deleting suite.volume if it has already been cleaned up
    
    integration-tests: lock mutex in Volume t.Cleanup
    
    * integration-tests: fixup -shutdown-grace-period-seconds CLI arg
    
    * integration-tests: update config to ShutdownDrainListenersEnabled for consistency
    
    * Update integration-tests/main_test.go
    
    
    
    * add changelog
    
    * pkg/envoy: add DumpConfig method and plumbing to optionally dump Envoy config to STDOUT on Envoy container during consul-dataplane shutdown
    
    * integration-tests: remove t.Cleanup attempt to dump Envoy config, proxy will already be terminated in current integration tests
    
    * cmd: abstract main logic into new run() func to handle context cancellation with fatal logging
    
    * test: implement DumpConfig stub on mock ProxyManager
    
    ---------
    
    Co-authored-by: Mike Morris <mikemorris@users.noreply.github.com>
    Co-authored-by: Paul Glass <pglass@hashicorp.com>

--- integration-tests/helpers/suite.go
+++ integration-tests/helpers/suite.go
@@ -201,11 +201,17 @@ func (s *Suite) Volume(t *testing.T) *Volume {
 		require.NoError(t, err)
 
 		t.Cleanup(func() {
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
+			s.mu.Lock()
+			defer s.mu.Unlock()
 
-			if err := docker.VolumeRemove(ctx, v.Name, true); err != nil {
-				t.Logf("failed to remove volume: %v", err)
+			if s.volume != nil {
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+
+				if err := docker.VolumeRemove(ctx, v.Name, true); err != nil {
+					t.Logf("failed to remove volume: %v", err)
+				}
+				s.volume = nil
 			}
 		})
 
