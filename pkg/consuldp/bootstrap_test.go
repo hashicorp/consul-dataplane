be4c06ae8d7e26834efff1d93dad68413192d6b2
Michael Wilkerson <62034708+wilkermichael@users.noreply.github.com>
2023-10-05 09:49:14 -0700

    updated proto-public (#272)

--- pkg/consuldp/bootstrap_test.go
+++ pkg/consuldp/bootstrap_test.go
@@ -15,7 +15,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/consul/proto-public/pbdataplane"
-	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v1alpha1"
+	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v2beta1"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/structpb"
