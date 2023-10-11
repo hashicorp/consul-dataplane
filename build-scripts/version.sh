5199d6eee3c143d4c6e875da9a75e5dacc2ea6e4
sarahethompson <sthompson@hashicorp.com>
2023-10-11 16:05:44 +0100

    update version make target

--- build-scripts/version.sh
+++ build-scripts/version.sh
@@ -4,8 +4,8 @@
 
 
 version_file=$1
-version=$(awk '$1 == "Version" && $2 == "=" { gsub(/"/, "", $3); print $3 }' < "${version_file}")
-prerelease=$(awk '$1 == "VersionPrerelease" && $2 == "=" { gsub(/"/, "", $3); print $3 }' < "${version_file}")
+version=$(awk -F- '{ print $1 }' < "${version_file}")
+prerelease=$(awk -F- '{ print $2 }' < "${version_file}")
 
 if [ -n "$prerelease" ]; then
     echo "${version}-${prerelease}"
