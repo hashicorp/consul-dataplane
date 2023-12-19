9029a4f2a5be496ffacea2732dca5129a83c2231
Nick Ethier <nethier@hashicorp.com>
2023-12-19 12:32:29 -0500

    pkg/hcp: simplify telem state tracking

--- pkg/hcp/telemetry/state.go
+++ pkg/hcp/telemetry/state.go
@@ -49,35 +49,8 @@ func NewStateTracker(client pbresource.ResourceServiceClient, logger hclog.Logge
 
 func (st *StateTracker) Run(ctx context.Context) <-chan struct{} {
 	notifyCh := make(chan struct{}, 1)
-	go st.runLoop(ctx, notifyCh)
+	go st.watchResource(ctx, notifyCh)
 	return notifyCh
-}
-
-func (st *StateTracker) runLoop(ctx context.Context, notifyCh chan struct{}) {
-	watchCh := make(chan *pbresource.WatchEvent, 1)
-	go st.watchResource(ctx, watchCh)
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case watchEv := <-watchCh:
-			st.stateMu.Lock()
-			switch watchEv.Operation {
-			case pbresource.WatchEvent_OPERATION_UPSERT:
-				st.state = resourceToState(watchEv.GetResource())
-			case pbresource.WatchEvent_OPERATION_DELETE:
-				st.state = nil
-			}
-			st.stateMu.Unlock()
-
-			select {
-			case <-ctx.Done():
-				return
-			case notifyCh <- struct{}{}:
-			default:
-			}
-		}
-	}
 }
 
 func (st *StateTracker) State() (state State, exists bool) {
@@ -94,10 +67,12 @@ func resourceToState(res *pbresource.Resource) *State {
 	if res == nil {
 		return nil
 	}
+
+	// TODO
 	return nil
 }
 
-func (st *StateTracker) watchResource(ctx context.Context, ch chan *pbresource.WatchEvent) {
+func (st *StateTracker) watchResource(ctx context.Context, ch chan struct{}) {
 	for {
 		stream, err := st.client.WatchList(ctx, &pbresource.WatchListRequest{
 			Type: &pbresource.Type{
@@ -121,7 +96,7 @@ func (st *StateTracker) watchResource(ctx context.Context, ch chan *pbresource.W
 	}
 }
 
-func (st *StateTracker) watchStream(stream pbresource.ResourceService_WatchListClient, ch chan *pbresource.WatchEvent) {
+func (st *StateTracker) watchStream(stream pbresource.ResourceService_WatchListClient, notifyCh chan struct{}) {
 	for {
 		ev, err := stream.Recv()
 		if err == io.EOF {
@@ -132,24 +107,20 @@ func (st *StateTracker) watchStream(stream pbresource.ResourceService_WatchListC
 			return
 		}
 
-		// In rare cases where watch events are produced faster than can be processed it is only required to consume
-		// the latest event. A single element buffered channel is used to queue the next event for consumption. If
-		// the channel is full and sending an event to the channel would block the event in the channel is popped
-		// and the latest event is sent. Since this is the only location that sends events to the channel it should
-		// in theory never block.
+		st.stateMu.Lock()
+		switch ev.Operation {
+		case pbresource.WatchEvent_OPERATION_UPSERT:
+			st.state = resourceToState(ev.GetResource())
+		case pbresource.WatchEvent_OPERATION_DELETE:
+			st.state = nil
+		}
+		st.stateMu.Unlock()
+
 		select {
 		case <-stream.Context().Done():
 			return
-		case ch <- ev:
+		case notifyCh <- struct{}{}:
 		default:
-			select {
-			case <-stream.Context().Done():
-				return
-			case <-ch:
-				ch <- ev
-			default:
-				ch <- ev
-			}
 		}
 	}
 }
