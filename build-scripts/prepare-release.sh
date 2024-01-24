b80f2bf4a4876789ae0b524275b8201e692b7177
Curt Bushko <cbushko@gmail.com>
2024-01-24 16:30:15 -0500

    Add some release scripts for prepare-release and prepare-dev (#399)

---
+++ build-scripts/prepare-release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+function sed_i {
+	if test "$(uname)" == "Darwin"; then
+		sed -i '' "$@"
+		return $?
+	else
+		sed -i "$@"
+		return $?
+	fi
+}
+
+if test "$(uname)" == "Darwin"; then
+	SED_EXT="-E"
+else
+	SED_EXT="-r"
+fi
+
+VFILE=$1
+VERSION=$2
+PRERELEASE=$3
+
+echo "==> Preparing consul-dataplane for release by updating ${VFILE} with version info: ${VERSION}"
+
+sed_i ${SED_EXT} -e "s/(Version[[:space:]]*=[[:space:]]*)\"[^\"]*\"/\1\"${VERSION}\"/g" -e "s/(VersionPrerelease[[:space:]]*=[[:space:]]*)\"[^\"]*\"/\1\"${PRERELEASE}\"/g" "${VFILE}"
