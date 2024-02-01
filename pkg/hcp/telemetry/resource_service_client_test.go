be5eea47c0f8661cf65e95edf2e624e0a1c53c2c
Joshua Timmons <joshua.timmons1@gmail.com>
2024-02-01 14:22:15 -0500

    Bump dependency

--- pkg/hcp/telemetry/resource_service_client_test.go
+++ pkg/hcp/telemetry/resource_service_client_test.go
@@ -116,6 +116,39 @@ func (_m *MockResourceServiceClient) ListByOwner(ctx context.Context, in *pbreso
 	return r0, r1
 }
 
+// MutateAndValidate provides a mock function with given fields: ctx, in, opts
+func (_m *MockResourceServiceClient) MutateAndValidate(ctx context.Context, in *pbresource.MutateAndValidateRequest, opts ...grpc.CallOption) (*pbresource.MutateAndValidateResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *pbresource.MutateAndValidateResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *pbresource.MutateAndValidateRequest, ...grpc.CallOption) (*pbresource.MutateAndValidateResponse, error)); ok {
+		return rf(ctx, in, opts...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *pbresource.MutateAndValidateRequest, ...grpc.CallOption) *pbresource.MutateAndValidateResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*pbresource.MutateAndValidateResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *pbresource.MutateAndValidateRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Read provides a mock function with given fields: ctx, in, opts
 func (_m *MockResourceServiceClient) Read(ctx context.Context, in *pbresource.ReadRequest, opts ...grpc.CallOption) (*pbresource.ReadResponse, error) {
 	_va := make([]interface{}, len(opts))
