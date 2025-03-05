0262aeb962d10b9cf1db1e94226247b337a0dacd
nitin-sachdev-29 <nitin.sachdev@hashicorp.com>
2025-03-05 18:18:03 +0530

    Triggering graceful startup if gracefulStartupSeconds is grtr than 0 (#687)
    * triggering graceful startup if gracefulStartupSeconds is grtr than 0 and Enhance logging
    
    * added changelog
    
    * Update pkg/consuldp/consul_dataplane.go
    
    Co-authored-by: Rahul <srahul3@gmail.com>
    
    ---------
    
    Co-authored-by: Rahul <srahul3@gmail.com>
    
    (cherry picked from commit 984e30f39b2b19ba81b8d88903e7bf097a817a69)

--- pkg/envoy/proxy.go
+++ pkg/envoy/proxy.go
@@ -392,7 +392,6 @@ func (p *Proxy) Ready() (bool, error) {
 		// Query ready endpoint to check if proxy is Ready
 		envoyReadyURL := fmt.Sprintf("http://%s:%v/ready", p.cfg.AdminAddr, p.cfg.AdminBindPort)
 		rsp, err := p.client.Get(envoyReadyURL)
-		defer rsp.Body.Close()
 		if err != nil {
 			p.cfg.Logger.Error("envoy: admin endpoint not available", "error", err)
 			return false, err
